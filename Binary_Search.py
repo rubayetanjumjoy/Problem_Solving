314af97909c0a3d9630f1af79e8f18ceabe131fc
rubayetanjumjoy <rubayetanjumjoy@gmail.com>
2022-12-05 15:00:23 +0600

    binary search

---
+++ Binary_Search.py
@@ -0,0 +1,22 @@
+def binarySearch(find,List):
+     
+     
+    f=0
+    mid=0
+    l=len(List)-1
+    while f<=l:
+        print(f,l)
+        mid=(f+l)//2 
+        print(mid,'mid')
+        # 2
+        
+        if List[mid]==find:
+            return List[mid],mid
+        if List[mid]>find:
+            l=mid
+            print('last',mid)
+        if List[mid]<find:
+            f=mid
+            print('first',mid)
+        
+print(binarySearch(5,[1,2,3,4,5,6,7,8]))
